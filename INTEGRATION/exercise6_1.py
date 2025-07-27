6746c8ea18b1f4bf68786d50c0dfc04e75adeda4
Aristodemos Michaelides <aristodemos.mich@gmail.com>
2025-07-27 16:53:49 +0300

    Update

--- INTEGRATION/exercise6_1.py
+++ INTEGRATION/exercise6_1.py
@@ -1,9 +1,3 @@
-def gp(msg="Quick update"):
-    import os
-    os.system('git add .')
-    os.system(f'git commit -m "{msg}"')
-    os.system('git push')
-    
 from trapezoidal import trapezoidal
 
 def trapezoidal_test():
