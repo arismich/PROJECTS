3485fad34141e6bd6dbcac1cecd173329aa65437
Aristodemos Michaelides <aristodemos.mich@gmail.com>
2025-07-27 09:14:54 +0300

    Update

--- INTEGRATION/exercise6_1.py
+++ INTEGRATION/exercise6_1.py
@@ -4,3 +4,19 @@ def gp(msg="Quick update"):
     os.system(f'git commit -m "{msg}"')
     os.system('git push')
     
+from trapezoidal import trapezoidal
+
+def trapezoida_test():
+    n = 2
+    f = lambda x: 2 * (x)**3
+    F = lambda x: 0.5 * (x)**4
+    
+    expected = F(3) - F(1)
+    numerical = trapezoidal(f, 1, 3, n)
+    error = abs(expected - numerical)
+    print("n={:d}: {:.4f}, error: {:g}".format(n, numerical, error))
+
+if __name__ == "__main__":
+    trapezoida_test()
+    
+gp("Update")
